6b5d6774667f6557a5a9cc214f91ab1db54a565d
ausipome@gmail.com <ausipome@gmail.com>
2023-11-02 15:39:33 +0000

    up

--- login_form_proccess.php
+++ login_form_proccess.php
@@ -2,8 +2,5 @@
 $old_name = session_name("some_name");
 session_set_cookie_params(0, '/', '.miie.co.uk');
 session_start();
-
-include('../../../server-config/iqconfig/secure-manage.php'); 
 include('../../../server-config/iqconfig/login_form_script.php'); 
-		
 ?>		
