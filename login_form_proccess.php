228b4f0d1a3d9b6d380e8673e819da83b81111bb
ausipome@gmail.com <ausipome@gmail.com>
2023-11-03 14:19:00 +0000

    up

--- login_form_proccess.php
+++ login_form_proccess.php
@@ -1,6 +1,6 @@
 <?php
 $old_name = session_name("some_name");
-session_set_cookie_params(0, '/', '.miie.co.uk');
+session_set_cookie_params(0, '/', '.instantq.co.uk');
 session_start();
 include('../../../server-config/iqconfig/login_form_script.php'); 
 ?>		
