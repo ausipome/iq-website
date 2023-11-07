36cecbc39f015eb4afdeddb2639c528d9a64ee4c
ausipome@gmail.com <ausipome@gmail.com>
2023-11-07 09:01:52 +0000

    up

--- index.php
+++ index.php
@@ -15,11 +15,18 @@ session_destroy();
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <link href="css/index.css" rel="stylesheet">
 <!--fonts-->
-<link href="css/fonts.css" rel="stylesheet">
-<link href="fontawesome/css/fontawesome.css" rel="stylesheet">
-<link href="fontawesome/css/brands.css" rel="stylesheet">
-<link href="fontawesome/css/solid.css" rel="stylesheet">	
-<link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro&display=swap" rel="stylesheet">  	 
+<link href="css/fonts.css" rel="stylesheet">	
+<link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro&display=swap" rel="stylesheet">  	
+
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
+
 </head>
 <body>
 <div id="mainImage" ><img src="images/quote_success.jpg" width="100%" alt="InstantQ quotation software"/>     
